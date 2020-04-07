6f86fb6d9deb5bfc4164cceff4abca11f8e7c080
Charles Lockhart <charleslockhart@Charless-MBP.attlocal.net>
2020-04-06 21:17:16 -0400

    end of day 3 of js learning

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log(typeof "Test");
+
+// Bindings
+
+let myBool = true, count = 0;
+
+console.log(myBool);
+
+// instead of let, there is also var and const
+// var was how we did what 'let' does before 2015, 
+// but const can still be used to easily refer to something aler
+console.log(count);
+const one = 1;
+count = count + one;
+console.log(count);
