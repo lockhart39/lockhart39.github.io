63d02f45ab42e22d1d5532aacd42faaee94e47af
Charles Lockhart <charleslockhart@Charless-MBP.attlocal.net>
2020-04-08 22:40:53 -0400

    end of ch 2

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
-console.log(typeof "Test");
+// console.log(typeof "Test");
 
 // Bindings
 
-let myBool = true, count = 0;
+// let myBool = true, count = 0;
 
-console.log(myBool);
+// console.log(myBool);
 
 // instead of let, there is also var and const
 // var was how we did what 'let' does before 2015, 
 // but const can still be used to easily refer to something aler
-console.log(count);
-const one = 1;
-count = count + one;
-console.log(count);
+// console.log(count);
+// const one = 1;
+// count = count + one;
+// console.log(count);
 
 // Number function converts input to a number
 
-let myInt = Number(prompt("Enter a number:"));
+// let myInt = Number(prompt("Enter a number:"));
 
-console.log("Your number was: " + myInt);
+// Conditional execution
+
+// if (myInt == 0) {
+//     console.log('The number was zero.');
+// } else if (myInt % 2 == 0) {
+//     console.log('The number was even.');
+// } else {
+//     console.log('The number was odd');
+// }
+
+// while & do
+
+let fake_num = 0;
+while (fake_num < 10) {
+    console.log(fake_num);
+    fake_num += 1;
+}
+
+// do forces the body to be executed at least once
+let user_pw;
+do {
+    user_pw = prompt('Enter a password: ');
+} while (!user_pw) {
+    console.log(user_pw);
+}
+
+// for loop
+
+for (let i = 0; i < 12; i += 1) {
+    console.log(i)
+}
