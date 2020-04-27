b3d5decf015e8723ef4d0ba3da65d1c3052180f9
Charles Lockhart <charleslockhart@Charless-MBP.attlocal.net>
2020-04-26 23:04:39 -0400

    experimenting with dates in JS

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-// eventually going to switch over creating a project for handling rdbms data
-// executing lookups and transformations
+// eventually going to switch over creating a project for handling ind data
 
-const bourbonMain = {
-    active: true, 
-    allocated: false,
-    one_time_release: false,
-    distilleries: ['Buffalo Trace', 'Woodford', 'Four Roses', 'Jim Beam', 'Brown Forman', 
-                    'Jim Beam', 'Heaven Hills'], 
-    addDistillery: function(name) {
-        if (bourbonMain.distilleries.includes(name) == false) {
-            bourbonMain.distilleries.push(name);
-        }
-    }
-    
-};
+// Working with dates
+var dateObj = new Date();
+let today = dateObj.getDate();
+
+var month = dateObj.getMonth();
+month = String(month);
+if (month.length < 2) {
+    month = "0" + month;
+}
+
+let year = dateObj.getFullYear();
+console.log(year + '-' + month + '-' + today);
 
 // Experimenting with JSON
 
@@ -22,51 +20,3 @@ let indStr = JSON.stringify({first_name: NaN, last_name: NaN, deceased: false, h
 
 console.log(indStr);
 console.log(JSON.parse(indStr).deceased);
-
-// while & do
-
-// let fake_num = 0;
-// while (fake_num < 10) {
-//     console.log(fake_num);
-//     fake_num += 1;
-// }
-
-// do forces the body to be executed at least once
-// let user_pw;
-// do {
-//     user_pw = prompt('Enter a password: ');
-// } while (!user_pw) {
-//     console.log(user_pw);
-// }
-
-// for loop
-
-// functions
-
-// const myFunc = function(x) {
-//     return x * x;
-// }
-
-// for (let i = 0; i < 12; i += 1) {
-//     console.log(myFunc(i));
-// }
-
-// declaration notation
-// with this notation you can define a function after calling it
-
-// console.log('Your number cubed: ', futureFunc(prompt('Enter a number:')));
-
-// function futureFunc(x) {
-//     return x * x * x;
-// }
-
-// // Some more string functionality 
-// function addLeadingZero(x) {
-//     x = String(x);
-//     while (x.length < 5) {
-//         x = "0" + x;
-//     }
-//     return x;
-// }
-
-// console.log('14 with leading zeros: ', addLeadingZero(14));
