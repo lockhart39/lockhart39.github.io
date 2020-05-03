9ad8410f62877b1d40ee585def956fb4fd8b2a0d
Charles Lockhart <charleslockhart@Charless-MBP.attlocal.net>
2020-05-02 23:02:10 -0400

    playing around with document methods

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
+var ava_img = document.getElementById('avatar_img');
+
+var toggle_counter = 0;
+
+function toggleImage() {
+    console.log(toggle_counter);
+    if (toggle_counter == 0) {
+        ava_img.style.top = 112 + "px";
+    }
+    if (toggle_counter == 1) {
+        ava_img.style.top = 62 + "px";
+    }
+    toggle_counter += 1;
+}
+
+ava_img.onclick = toggleImage;
+
+var avatar = document.getElementById('avatar_div');
+
 // eventually going to switch over creating a project for handling ind data
 
 // Working with dates
 var dateObj = new Date();
 let today = dateObj.getDate();
+today = String(today);
+if (today.length < 2) {
+    today = "0" + today;
+}
 
 var month = dateObj.getMonth();
 month = String(month);
